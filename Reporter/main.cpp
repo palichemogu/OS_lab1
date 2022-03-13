96ff18f4b51f6f7ec7f2135299c8bf5d95a56487
Ivan Lubetskiy <ivanlubetskiy@gmail.com>
2022-03-13 11:55:37 +0300

    Creator and Reporter without Main

--- Reporter/main.cpp
+++ Reporter/main.cpp
@@ -11,18 +11,32 @@ int main() {
     FILE* in;
     FILE* out;
     employee s;
-    if (!(in = fopen("ggg.bin", "rb")))
+    char nameOfBinFile[10];
+    char nameOfTxtFile[10];
+    double paymentPerHour;
+    printf("%s","Enter bin file name:");
+    scanf("%s", nameOfBinFile);
+    printf("%s","Enter txt file name:");
+    scanf("%s", nameOfTxtFile);
+    printf("%s", "Enter payment per hour:");
+    scanf("%lf", &paymentPerHour);
+    if (!(in = fopen(nameOfBinFile, "rb")))
     {
-        printf("Opening file failed.\n");
+        printf("Opening bin file failed.\n");
         return 1;
     }
-    printf("%s\n", "Number		Name		Hours		Salary");
+    if (!(out = fopen(nameOfTxtFile,"w"))) {
+        printf("Opening txt file failed.\n");
+        return 1;
+    }
+    fprintf(out, "%s: %s\n", "Report of file", nameOfBinFile);
+    fprintf(out,"%s", "Number        Name           Hours        Salary\n");
     for(int i = 0; ; i++){
         fseek(in, i*sizeof(struct employee), SEEK_SET);
         if (!fread(&s, sizeof(employee), 1, in)){
             break;
         }
-        printf("%d \t\t %s \t\t %lf\n", s.num, s.name, s.hours);
+        fprintf(out,"%d\t\t%s\t\t%lf\t\t%lf\n", s.num, s.name, s.hours, s.hours * paymentPerHour);
     }
     fclose(in);
     return 0;
